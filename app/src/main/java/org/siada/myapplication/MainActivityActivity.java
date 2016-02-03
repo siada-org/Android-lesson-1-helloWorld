2c3417bd5dd57964dc95677e5e33908ad5d61fec
Ashder <it.inhp@gmail.com>
2016-02-03 11:38:55 +0600

    Изменено название приложения
    Изменено название приложения;
    Добавлены комментрарии

--- app/src/main/java/org/siada/myapplication/MainActivityActivity.java
+++ app/src/main/java/org/siada/myapplication/MainActivityActivity.java
@@ -1,13 +1,21 @@
+//Название пакета, в который входит данный класс
 package org.siada.myapplication;
 
+//Секция импорта пакетов, используемых данным классом
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//!Секция импорта пакетов, используемых данным классом
+
+//Видимость класса, его назввание и имя класса от которого тот наследуется
 public class MainActivityActivity extends AppCompatActivity {
 
+    //Действия класса при создании
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Наследование от суперкласса "onCreate"
         super.onCreate(savedInstanceState);
+        //Запуск лайоута
         setContentView(R.layout.main_activity);
     }
 }
